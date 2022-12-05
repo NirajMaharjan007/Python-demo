38a128a8b5a67267ce59037a0b02ffa8b3e1163c
NirajMaharjan007 <59794234+NirajMaharjan007@users.noreply.github.com>
2022-12-05 20:55:25 +0545

    updated

--- test/ExampleTest.py
+++ test/ExampleTest.py
@@ -2,7 +2,7 @@
 try:
     num = int(input("Enter a number: "))
     if (num <= 0):
-        raise Exception("The number is negative or zero!")
+        raise Exception("The number is negative or zero!")  # Exception message
 
     elif (num % 2) == 0:
         print("The number is even!")
