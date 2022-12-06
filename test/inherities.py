b5ced4f1e49cfa9573905fe1d10103b11b040594
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-12-06 15:42:48 +0545

    Added py

---
+++ test/inherities.py
@@ -0,0 +1,26 @@
+class Animal():
+    def __init__(self, name):
+        self.name = name
+        print("Animal name: " + self.name)
+
+    def walk(self):
+        print(self.name + " walks")
+
+    def eat(self):
+        print(self.name + " eats")
+
+
+class Dog(Animal):
+    def bark(self):
+        print(self.name + " bark")
+
+
+class Cat(Animal):
+    def meow(self):
+        print(self.name + " meow")
+
+
+dog = Dog("Doggy")
+
+dog.bark()
+dog.eat()
