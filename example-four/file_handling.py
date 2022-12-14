d17f091d6a4366b2ae6deb0151f0fd7ee009210d
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-12-14 08:43:17 +0545

    Commited

--- example-four/file_handling.py
+++ example-four/file_handling.py
@@ -1,4 +1,18 @@
 f = open("example-four/file.txt", "r")
 
 print()
+'''
+    f.read(5) => parameter shows size
+    f.readLine()
+    f.readLines()
+'''
 print(f.read())
+
+print()
+
+file_append = open("example-four/file.txt", "a")
+file_append.write("\nHello")
+
+# w means overwrite the files
+# file_write = open("example-four/file.txt", "w")
+# file_write.write("")
