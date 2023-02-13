72fc3510407a9399139400b5d55892236448cbf3
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2023-02-13 08:32:20 +0545

    Update example-nine

--- example-nine/ex_seven.py
+++ example-nine/ex_seven.py
@@ -1,11 +1,15 @@
 from matplotlib.pyplot import *
 from numpy import *
 
-arr_one = array([7, 2, 4, 2, 8, 7, 6])
-arr_two = array([3, 4, 8, 8, 7, 9, 3])
+arr_one = array([7, 2, 4, 3, 8, 5, 6])
+arr_two = array([6, 9, 10, 18, 45, 65, 32])
 
 arr_one.sort()
 arr_two.sort()
 
-bar(arr_one, arr_two)
+
+bar(arr_one, arr_two, width=1.2)
+show()
+
+barh(arr_one, arr_two, height=2.6)
 show()
